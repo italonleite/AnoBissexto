72270915cb49724775608b9425002d7e6005a3df
Italo Nunes Leite <italonleite@gmail.com>
2020-01-24 22:23:42 -0300

    Add project files.

---
+++ AnoBissexto/Ano.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnoBissexto
+{
+    public class Ano
+    {
+        private int Numero { get; set; }
+
+        public Ano()
+        {
+
+        }
+
+        public string CalcularAno(int numero) {
+            if (numero % 4 == 0) {
+                int NumeroDivisivelPorQuatro = numero / 4;
+
+                if (NumeroDivisivelPorQuatro % 100 == 0)
+                {
+
+                    string ebissexto = "O ANO É BISSEXTO";
+                    return ebissexto;
+                }
+                else {
+                    string ebissexto = "O ANO É BISSEXTO";
+                    return ebissexto;
+                }
+               
+
+            }
+            string naoebissexto = "O ANO NÃO É BISSEXTO";
+            return naoebissexto;
+            
+        }
+    }
+}
---
+++ AnoBissexto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AnoBissexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ano ano = new Ano();
+            var resultado = ano.CalcularAno(2011);
+            Console.WriteLine(resultado.ToString());
+            Console.ReadKey();
+        }
+    }
+}
